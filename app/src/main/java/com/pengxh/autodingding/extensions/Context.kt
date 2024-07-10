edfb44745961ae1f23dad41a6fa342b87bf7a405
Trojx <raojianxun@126.com>
2024-07-10 11:04:21 +0800

    2024-07-10 save

--- app/src/main/java/com/pengxh/autodingding/extensions/Context.kt
+++ app/src/main/java/com/pengxh/autodingding/extensions/Context.kt
@@ -5,6 +5,8 @@ import android.content.Context
 import android.content.Intent
 import android.content.pm.ApplicationInfo
 import android.content.pm.ResolveInfo
+import android.os.PowerManager
+import android.util.Log
 import androidx.core.app.NotificationManagerCompat
 import com.pengxh.autodingding.utils.Constant
 import com.pengxh.autodingding.utils.CountDownTimerManager
@@ -22,6 +24,9 @@ fun Context.notificationEnable(): Boolean {
  * 检查手机上是否安装了指定的软件
  */
 fun Context.isAppAvailable(packageName: String): Boolean {
+    if (packageName == Constant.DING_DING) {
+        return true
+    }
     val packageManager = this.packageManager
     //获取所有已安装程序的包信息
     val packages = packageManager.getInstalledPackages(0)
@@ -39,6 +44,7 @@ fun Context.isAppAvailable(packageName: String): Boolean {
  * 打开指定包名的apk
  */
 fun Context.openApplication(packageName: String) {
+    Log.d("Context", "openApplication: $packageName")
     val packageManager = this.packageManager
     val resolveIntent = Intent(Intent.ACTION_MAIN, null)
     resolveIntent.addCategory(Intent.CATEGORY_LAUNCHER)
@@ -56,9 +62,29 @@ fun Context.openApplication(packageName: String) {
     val cn = ComponentName(packageName, className)
     intent.component = cn
     this.startActivity(intent)
+    Log.d("Context", "openApplication: $packageName intent=$intent")
     //倒计时，记录在钉钉界面停留的时间，超过设定的超时时间，自动回到打卡工具，并记录异常日志
     val time = SaveKeyValues.getValue(Constant.TIMEOUT, "15s") as String
     //去掉时间的s
     val timeValue = time.dropLast(1).toInt()
     CountDownTimerManager.get.startTimer(this, timeValue * 1000L, 1000)
+}
+
+/**
+ * 亮屏
+ * https://cloud.tencent.com/developer/article/1591887
+ */
+fun Context.wakeScreen() {
+    // 获取电源管理器对象
+    val pm = this.getSystemService(Context.POWER_SERVICE) as PowerManager
+    val screenOn = pm.isScreenOn
+    if (!screenOn) {
+        // 获取PowerManager.WakeLock对象,后面的参数|表示同时传入两个值,最后的是LogCat里用的Tag
+        val wl = pm.newWakeLock(
+            PowerManager.ACQUIRE_CAUSES_WAKEUP or
+                    PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "AutoDD:bright"
+        )
+        wl.acquire(10000) // 点亮屏幕
+        wl.release() // 释放
+    }
 }
--- app/src/main/java/com/pengxh/autodingding/service/FloatingWindowService.kt
+++ app/src/main/java/com/pengxh/autodingding/service/FloatingWindowService.kt
@@ -4,15 +4,23 @@ import android.annotation.SuppressLint
 import android.app.Service
 import android.content.Intent
 import android.graphics.PixelFormat
+import android.icu.util.Calendar
 import android.os.Build
 import android.os.IBinder
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.WindowManager
+import androidx.annotation.RequiresApi
 import com.pengxh.autodingding.R
+import com.pengxh.autodingding.extensions.openApplication
+import com.pengxh.autodingding.extensions.wakeScreen
+import com.pengxh.autodingding.utils.Constant
 import com.pengxh.kt.lite.extensions.getSystemService
 import com.pengxh.kt.lite.extensions.show
+import kotlinx.coroutines.*
+import kotlin.time.Duration
 
 
 class FloatingWindowService : Service() {
@@ -21,6 +29,8 @@ class FloatingWindowService : Service() {
     private val windowManager by lazy { getSystemService<WindowManager>() }
     private val layoutInflater by lazy { LayoutInflater.from(this) }
     private var floatView: View? = null
+    private lateinit var mFixedTimeJob: Job
+    private val coroutineScope = CoroutineScope(Dispatchers.Default)
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -29,6 +39,7 @@ class FloatingWindowService : Service() {
     override fun onCreate() {
         super.onCreate()
         floatView = layoutInflater.inflate(R.layout.window_floating, null)
+        startFixedTimeJob()
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -37,6 +48,28 @@ class FloatingWindowService : Service() {
         }
         initFloatingView(floatView)
         return START_STICKY
+    }
+
+    private fun startFixedTimeJob() {
+        mFixedTimeJob = coroutineScope.launch {
+            while (isActive) {
+                delay(Duration.parse("50s"))
+                val calendar = Calendar.getInstance()
+                val hour = calendar.get(Calendar.HOUR_OF_DAY)
+                val minute = calendar.get(Calendar.MINUTE)
+                if ((hour == 8 && minute == 30)
+                    || (hour == 12 && minute == 0)
+                    || (hour == 2 && minute == 30)
+                    || (hour == 5 && minute == 30)
+                ) {
+                    this@FloatingWindowService.wakeScreen()
+                    openApplication(Constant.DING_DING)
+                } else {
+                    openApplication(Constant.DING_DING)
+                    Log.d(kTag, "startFixedTimeJob: not on time: $hour:$minute")
+                }
+            }
+        }
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -83,7 +116,8 @@ class FloatingWindowService : Service() {
                 false
             }
             view?.setOnClickListener {
-                "无实际功能，仅为绕过Android 10+系统打卡之后无法回到桌面的问题".show(this)
+//                "无实际功能，仅为绕过Android 10+系统打卡之后无法回到桌面的问题".show(this)
+                "Trojx ❤️ Mandy".show(this)
             }
         } catch (e: IllegalStateException) {
             e.printStackTrace()
